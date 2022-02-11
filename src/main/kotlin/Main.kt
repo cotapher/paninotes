752743e64426a4cf8da02dd8b1184a4497841a73
Kevin Li <34748712+Kleanur@users.noreply.github.com>
2022-02-11 15:22:55 -0500

    aurorawindow

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,30 +3,27 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
+import org.pushingpixels.aurora.theming.marinerSkin
+import org.pushingpixels.aurora.window.AuroraWindow
+import org.pushingpixels.aurora.window.auroraApplication
+
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
+//    TopAppBar(
+//        title = { Text(text = "Menu Bar") }
+//    )
 }
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+fun main() = auroraApplication {
+    AuroraWindow(
+        skin = marinerSkin(),
+        title = "Aurora Demo",
+        onCloseRequest = ::exitApplication
+    ) {
         App()
     }
 }
