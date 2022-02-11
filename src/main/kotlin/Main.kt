6f651020ff6458c3a2959a4f4deab323757d67cd
Kevin Li <34748712+Kleanur@users.noreply.github.com>
2022-02-11 15:44:51 -0500

    added menu bar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,9 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import org.pushingpixels.aurora.component.model.Command
+import org.pushingpixels.aurora.component.model.CommandGroup
+import org.pushingpixels.aurora.component.model.CommandMenuContentModel
 import org.pushingpixels.aurora.theming.marinerSkin
 import org.pushingpixels.aurora.window.AuroraWindow
 import org.pushingpixels.aurora.window.auroraApplication
@@ -22,7 +25,32 @@ fun main() = auroraApplication {
     AuroraWindow(
         skin = marinerSkin(),
         title = "Aurora Demo",
-        onCloseRequest = ::exitApplication
+        onCloseRequest = ::exitApplication,
+        menuCommands = CommandGroup(
+            commands = listOf(
+                Command(
+                    text = "File",
+                    secondaryContentModel = CommandMenuContentModel(
+                        CommandGroup(
+                            commands = listOf(
+                                Command(
+                                    text = "New",
+                                    action = { println("New file!") }),
+                                Command(
+                                    text = "Open",
+                                    action = { println("Open file!") }),
+                                Command(
+                                    text = "Save",
+                                    action = { println("Save file!") })
+                            )
+                        )
+                    )
+                ),
+                Command(
+                    text = "Edit",
+                    action = { println("Edit activated!") })
+            )
+        )
     ) {
         App()
     }
