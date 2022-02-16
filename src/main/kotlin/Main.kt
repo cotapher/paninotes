4b04d5ea029a7790f1969e445318ca16a227667d
Kelly Jia <k7jia@edu.uwaterloo.ca>
2022-02-15 22:39:12 -0500

    Refactor the top menu out of the main method, and into its own file and class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,18 +2,9 @@
 import javafx.application.Application
 import javafx.geometry.Insets
 import javafx.scene.Scene
-import javafx.scene.control.Menu
-import javafx.scene.control.MenuBar
-import javafx.scene.control.MenuItem
-import javafx.scene.control.RadioMenuItem
-import javafx.scene.input.KeyCode
-import javafx.scene.input.KeyCodeCombination
-import javafx.scene.input.KeyCombination
 import javafx.scene.layout.BorderPane
-import javafx.scene.layout.VBox
 import javafx.scene.web.HTMLEditor
 import javafx.stage.Stage
-import kotlin.system.exitProcess
 
 class Main : Application() {
 
@@ -23,61 +14,13 @@ class Main : Application() {
 
         // Initialize all widgets--------------------------------------------------------------------------------------------
         val layout = BorderPane()
-
-        // top: menubar----------------------------------------------------------------------
-        val menuSection = VBox()
-        menuSection.isFocusTraversable = false
-        val menuBar = MenuBar()
-        menuBar.isFocusTraversable = false
-        val fileMenu = Menu("File")
-        val fileQuit = MenuItem("Quit")
-        val viewMenu = Menu("View")
-        val viewHome = MenuItem("Home")
-        val viewPrev = MenuItem("Prev")
-        val viewNext = MenuItem("Next")
-        val actionMenu = Menu("Action")
-        val actionRename = MenuItem("Rename")
-        val actionMove = MenuItem("Move")
-        val actionDelete = MenuItem("Delete")
-        val optionMenu = Menu("Option")
-        val optionHiddenToggle = RadioMenuItem("Show hidden")
-
-
-        //------------------------------------
-        menuBar.menus.add(fileMenu)
-        fileMenu.items.add(fileQuit)
-        // handle file->quit
-        fileQuit.setOnAction {
-            exitProcess(0)
-        }
-
-        // Add a shortcut CTRL+Q for file->quit
-        fileQuit.accelerator = KeyCodeCombination(KeyCode.Q, KeyCombination.CONTROL_DOWN)
-
-        menuBar.menus.add(viewMenu)
-        viewMenu.items.add(viewHome)
-
-        viewMenu.items.add(viewPrev)
-
-        viewMenu.items.add(viewNext)
-
-        menuBar.menus.add(actionMenu)
-        actionMenu.items.add(actionRename)
-
-        actionMenu.items.add(actionMove)
-
-        actionMenu.items.add(actionDelete)
-
-        menuBar.menus.add(optionMenu)
-        optionMenu.items.add(optionHiddenToggle)
-
-        menuSection.children.add(menuBar)
+        val topMenuView = TopMenuView(stage)
 
         val htmlEditor = HTMLEditor()
         htmlEditor.htmlText = "Hello <a href=\"https://github.com/TestFX/TestFX\">world</a>"
 
         // build the scene graph
-        layout.top = menuSection
+        layout.top = topMenuView
         layout.center = htmlEditor
         layout.padding = Insets(5.0)
 
---
+++ src/main/kotlin/TopMenuView.kt
@@ -0,0 +1,78 @@
+import javafx.scene.control.Menu
+import javafx.scene.control.MenuBar
+import javafx.scene.control.MenuItem
+import javafx.scene.input.KeyCode
+import javafx.scene.input.KeyCodeCombination
+import javafx.scene.input.KeyCombination
+import javafx.scene.layout.Pane
+import javafx.stage.Stage
+import kotlin.system.exitProcess
+
+class TopMenuView(stage: Stage) : Pane() {
+    private val stage: Stage?
+
+    var fileMenu: Menu? = null
+    var fileQuit: MenuItem? = null
+    var viewMenu: Menu? = null
+    var viewHome: MenuItem? = null
+    var viewPrev: MenuItem? = null
+    var viewNext: MenuItem? = null
+    var actionMenu: Menu? = null
+    var actionRename: MenuItem? = null
+    var actionMove: MenuItem? = null
+    var actionDelete: MenuItem? = null
+    var optionMenu: Menu? = null
+
+    init {
+        this.stage = stage
+
+        this.layoutView()
+        this.registerActions()
+    }
+
+    // TODO - add the actual menu items into here
+    private fun layoutView() {
+        val menuBar = MenuBar()
+
+        // File: Quit
+        fileMenu = Menu("File")
+        fileQuit = MenuItem("Quit")
+        fileMenu!!.items.add(fileQuit)
+        menuBar.menus.add(fileMenu)
+
+        // View: Home, Prev Next
+        viewMenu = Menu("View")
+        viewHome = MenuItem("Home")
+        viewPrev = MenuItem("Prev")
+        viewNext = MenuItem("Next")
+        viewMenu!!.items.add(viewHome)
+        viewMenu!!.items.add(viewPrev)
+        viewMenu!!.items.add(viewNext)
+        menuBar.menus.add(viewMenu)
+
+        // Action: Rename, Move, Delete
+        actionMenu = Menu("Action")
+        actionRename = MenuItem("Rename")
+        actionMove = MenuItem("Move")
+        actionDelete = MenuItem("Delete")
+        actionMenu!!.items.add(actionRename)
+        actionMenu!!.items.add(actionMove)
+        actionMenu!!.items.add(actionDelete)
+        menuBar.menus.add(actionMenu)
+
+        // Option:
+        optionMenu = Menu("Option")
+        menuBar.menus.add(optionMenu)
+
+        this.children.add(menuBar)
+    }
+
+    private fun registerActions() {
+        fileQuit!!.setOnAction {
+            exitProcess(0)
+        }
+
+        // Add a shortcut CTRL+Q for file->quit
+        fileQuit!!.accelerator = KeyCodeCombination(KeyCode.Q, KeyCombination.CONTROL_DOWN)
+    }
+}
