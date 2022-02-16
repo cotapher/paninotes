3d08795aa781ed431715c229f157002ab8df5098
Cotapher <xhxie@uwaterloo.ca>
2022-02-16 10:49:41 -0500

    initialize on declaration

--- src/main/kotlin/TopMenuView.kt
+++ src/main/kotlin/TopMenuView.kt
@@ -5,10 +5,9 @@ import javafx.scene.input.KeyCode
 import javafx.scene.input.KeyCodeCombination
 import javafx.scene.input.KeyCombination
 import javafx.scene.layout.Pane
-import javafx.stage.Stage
 import kotlin.system.exitProcess
 
-class TopMenuView(val stage: Stage) : Pane() {
+class TopMenuView : Pane() {
 
     init {
         this.layoutView()
@@ -16,43 +15,31 @@ class TopMenuView(val stage: Stage) : Pane() {
 
     // TODO - add the actual menu items into here
     private fun layoutView() {
-        val fileMenu: Menu
-
-        val viewMenu: Menu
-        val viewHome: MenuItem
-        val viewPrev: MenuItem
-        val viewNext: MenuItem
-        val actionMenu: Menu
-        val actionRename: MenuItem
-        val actionMove: MenuItem
-        val actionDelete: MenuItem
-        val optionMenu: Menu
 
 
         val menuBar = MenuBar()
 
         // File: Quit
-        val fileQuit: MenuItem
-        fileMenu = Menu("File")
-        fileQuit = MenuItem("Quit")
+        val fileMenu = Menu("File")
+        val fileQuit = MenuItem("Quit")
         fileMenu.items.add(fileQuit)
         menuBar.menus.add(fileMenu)
 
         // View: Home, Prev Next
-        viewMenu = Menu("View")
-        viewHome = MenuItem("Home")
-        viewPrev = MenuItem("Prev")
-        viewNext = MenuItem("Next")
+        val viewMenu = Menu("View")
+        val viewHome = MenuItem("Home")
+        val viewPrev = MenuItem("Prev")
+        val viewNext = MenuItem("Next")
         viewMenu.items.add(viewHome)
         viewMenu.items.add(viewPrev)
         viewMenu.items.add(viewNext)
         menuBar.menus.add(viewMenu)
 
         // Action: Rename, Move, Delete
-        actionMenu = Menu("Action")
-        actionRename = MenuItem("Rename")
-        actionMove = MenuItem("Move")
-        actionDelete = MenuItem("Delete")
+        val actionMenu = Menu("Action")
+        val actionRename = MenuItem("Rename")
+        val actionMove = MenuItem("Move")
+        val actionDelete = MenuItem("Delete")
         actionMenu.items.add(actionRename)
         actionMenu.items.add(actionMove)
         actionMenu.items.add(actionDelete)
@@ -67,7 +54,7 @@ class TopMenuView(val stage: Stage) : Pane() {
         fileQuit.accelerator = KeyCodeCombination(KeyCode.Q, KeyCombination.CONTROL_DOWN)
 
         // Option:
-        optionMenu = Menu("Option")
+        val optionMenu = Menu("Option")
         menuBar.menus.add(optionMenu)
 
         this.children.add(menuBar)
