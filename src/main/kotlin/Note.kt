f54b3cb873bc75281835afccebdc4ddfd51e422c
Cotapher <xhxie@uwaterloo.ca>
2022-03-04 14:39:38 -0500

    reset server on file disk

--- src/main/kotlin/Note.kt
+++ src/main/kotlin/Note.kt
@@ -1,6 +1,10 @@
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import java.io.File
-
+@JsonSerialize
+@JsonDeserialize
 class Note(title: String) {
+    var id:Int? = null
     var title: String = ""
     var filePath: File? = null
 
--- src/server/src/main/kotlin/com/paninotes/server/NoteRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.paninotes.server
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-@Repository("noteRepository")
-interface NoteRepository : CrudRepository<Note, Int> {
-}
