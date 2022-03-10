9352c886f3c29a62cb649ed8718156ff1fe5aa6e
Kelly Jia <k7jia@uwaterloo.ca>
2022-03-09 22:26:48 -0500

    Remove the Open option in the File menu

--- PaninotesClient/src/main/kotlin/TopMenuView.kt
+++ PaninotesClient/src/main/kotlin/TopMenuView.kt
@@ -35,7 +35,6 @@ class TopMenuView(val model: Model, val htmlEditor: HTMLEditor,val stage: Stage)
         // File: Quit
         val fileMenu = Menu("File")
         val fileNewNote = createAddToMenu(fileMenu,"New Note")
-        val fileOpen = createAddToMenu(fileMenu,"Open")
         val fileSave = createAddToMenu(fileMenu,"Save")
         val fileQuit = createAddToMenu(fileMenu,"Quit")
         menuBar.menus.add(fileMenu)
@@ -48,7 +47,6 @@ class TopMenuView(val model: Model, val htmlEditor: HTMLEditor,val stage: Stage)
 
         fileMenu.id = "menu-fileMenu"
         fileNewNote.id = "menuitem-fileNewNote"
-        fileOpen.id = "menuitem-fileOpen"
         fileSave.id = "menuitem-fileSave"
         fileQuit.id = "menuitem-fileQuit"
         optionMenu.id = "menu-optionMenu"
@@ -83,16 +81,6 @@ class TopMenuView(val model: Model, val htmlEditor: HTMLEditor,val stage: Stage)
             }
         }
 
-        fileOpen.setOnAction {
-//            val fileDialog = FileChooser()
-//            fileDialog.title = "Select an HTML File"
-//            fileDialog.initialDirectory = model.testNotebookDir
-//            val extFilter = FileChooser.ExtensionFilter("HTML files (*.html)", "*.html")
-//            fileDialog.extensionFilters.add(extFilter)
-//            val file: File? = fileDialog.showOpenDialog(stage)
-//            model.openNote(file) TODO
-        }
-
         fileSave.setOnAction {
             print(htmlEditor.htmlText)
             model.currentNote?.saveNote(htmlEditor.htmlText)
@@ -106,7 +94,6 @@ class TopMenuView(val model: Model, val htmlEditor: HTMLEditor,val stage: Stage)
         // Add a shortcut CTRL+Q for file->quit
         fileNewNote.accelerator = KeyCodeCombination(KeyCode.N, KeyCombination.CONTROL_DOWN)
         //need new directory, open directory
-        fileOpen.accelerator = KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_DOWN)
         fileSave.accelerator = KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN)
         fileQuit.accelerator = KeyCodeCombination(KeyCode.Q, KeyCombination.CONTROL_DOWN)
 
