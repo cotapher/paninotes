004f6bc388b736bc9118da2ed3b9fe7ddff23ee8
Cotapher <xhxie@uwaterloo.ca>
2022-03-11 11:52:44 -0500

    Refactor to service

--- PaninotesServer/src/main/kotlin/com/paninotes/paninotesserver/Controller.kt
+++ PaninotesServer/src/main/kotlin/com/paninotes/paninotesserver/Controller.kt
@@ -2,80 +2,35 @@ package com.paninotes.paninotesserver
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
-import java.util.*
 
 
 @RestController
 class Controller {
     @Autowired
-    private val noteRepository: NoteRepository? = null
-    @Autowired
-    private val notebookRepository: NotebookRepository? = null
+    private val noteService: NoteService? = null
     @GetMapping("/")
     @ResponseBody
-    fun getAllNotes(): MutableList<NoteListResponse>? {
-        val noteList = noteRepository?.findAll()?.toMutableList()
-        val response = NoteListResponse(noteList)
-        return Collections.singletonList(response)
-    }
-
-    @PostMapping("/new")
-    @ResponseBody
-    fun backupNote(@RequestBody newNote:Note): String{
-        noteRepository?.save(newNote)
-        return "Note Saved"
+    fun getAllNotes(): NoteListResponse {
+        return noteService!!.getAllNotes()
     }
 
     @GetMapping("/notebooks")
     @ResponseBody
     fun getAllNotebooks(): NotebookListResponse {
-
-//        return NotebookListResponse(notebookList)
-        return NotebookListResponse(notebookRepository?.findAll()!!.toMutableList())
+        return noteService!!.getAllNotebooks()
     }
 
     @PostMapping("/backupNotebook")
     @ResponseBody
     fun backupNotebook(@RequestBody newNotebook:Notebook): Notebook? {
-        val matchingNotebooks: MutableList<Notebook>? = notebookRepository?.findByTitle(newNotebook.title)
-        if(matchingNotebooks?.size == 0) {
-            println("Notebook not found by title, inserting into db")
-            //associate notes with notebooks
-//            newNotebook.notes?.forEach { it.notebook = newNotebook }
-            //return the notes with ids
-            return notebookRepository?.save(newNotebook)
-
-        } else {
-            //there should only be one make
-            println("Notebook exist")
-
-            val matchingNotebook = matchingNotebooks!!.first()
-            val matchingNotebookNotes = matchingNotebook.notes!!
-            // Delete the old notes in that notebook in the database
-            matchingNotebookNotes.forEach {
-                noteRepository?.deleteById(it.id!!)
-            }
-            /*
-            //compare which note objects have changed
-            val newNotes = newNotebook.notes!!
-            val merged = (matchingNotebookNotes union newNotes.toSet())
-            val newNoteList = merged.toMutableList()
-             */
-
-            matchingNotebookNotes.clear()
-            matchingNotebookNotes.addAll(newNotebook.notes!!)
-
-            return notebookRepository?.save(matchingNotebook)
-        }
+        return noteService!!.backupNotebook(newNotebook)
 
     }
 
     @GetMapping("/deleteAll")
     @ResponseBody
     fun deleteAllData(): String {
-        notebookRepository?.deleteAll()
-        noteRepository?.deleteAll()
-        return "ALL DATA DELETED"
+        return noteService!!.deleteAllData()
     }
 
 }
---
+++ PaninotesServer/src/main/kotlin/com/paninotes/paninotesserver/NoteService.kt
@@ -0,0 +1,61 @@
+package com.paninotes.paninotesserver
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class NoteService {
+    @Autowired
+    private val noteRepository: NoteRepository? = null
+    @Autowired
+    private val notebookRepository: NotebookRepository? = null
+    fun getAllNotes(): NoteListResponse {
+        val noteList = noteRepository?.findAll()?.toMutableList()
+        val response = NoteListResponse(noteList)
+        return response
+    }
+
+    fun getAllNotebooks(): NotebookListResponse {
+
+//        return NotebookListResponse(notebookList)
+        return NotebookListResponse(notebookRepository?.findAll()!!.toMutableList())
+    }
+
+    fun backupNotebook(newNotebook: Notebook): Notebook? {
+        val matchingNotebooks: MutableList<Notebook>? = notebookRepository?.findByTitle(newNotebook.title)
+        if(matchingNotebooks?.size == 0) {
+            println("Notebook not found by title, inserting into db")
+            //associate notes with notebooks
+//            newNotebook.notes?.forEach { it.notebook = newNotebook }
+            //return the notes with ids
+            return notebookRepository?.save(newNotebook)
+
+        } else {
+            //there should only be one make
+            println("Notebook exist")
+
+            val matchingNotebook = matchingNotebooks!!.first()
+            val matchingNotebookNotes = matchingNotebook.notes!!
+            // Delete the old notes in that notebook in the database
+            matchingNotebookNotes.forEach {
+                noteRepository?.deleteById(it.id!!)
+            }
+            /*
+            //compare which note objects have changed
+            val newNotes = newNotebook.notes!!
+            val merged = (matchingNotebookNotes union newNotes.toSet())
+            val newNoteList = merged.toMutableList()
+             */
+
+            matchingNotebookNotes.clear()
+            matchingNotebookNotes.addAll(newNotebook.notes!!)
+
+            return notebookRepository?.save(matchingNotebook)
+        }
+    }
+    fun deleteAllData(): String {
+        notebookRepository?.deleteAll()
+        noteRepository?.deleteAll()
+        return "ALL DATA DELETED"
+    }
+}
